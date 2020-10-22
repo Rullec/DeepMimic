c73e11a909e1001f0b636731c55b2d62e5b475a2
Rullec <1009694460@qq.com>
2020-10-22 14:39:00 +0800

    begin path init good weight rl param search on 111

--- scripts/batch_begin.py
+++ scripts/batch_begin.py
@@ -1,25 +1,24 @@
 import os
 kernel_num = 2
 arg_file_lst = [
-    "args/1019/param_search/search_0.5_16.txt",
-    "args/1019/param_search/search_0.5_24.txt",
-    "args/1019/param_search/search_0.5_32.txt",
-    "args/1019/param_search/search_1_16.txt",
-    "args/1019/param_search/search_1_24.txt",
-    "args/1019/param_search/search_1_32.txt"]
+    "args/1021/path_init_param_search/search_0.5_16.txt",
+    "args/1021/path_init_param_search/search_0.5_24.txt",
+    "args/1021/path_init_param_search/search_1_16.txt",
+    "args/1021/path_init_param_search/search_1_24.txt"
+]
 
 cmd_lst = []
 # set up the log and error
 for arg_file_path in arg_file_lst:
-    arg_file =  os.path.split(arg_file_path)[-1]
+    arg_file = os.path.split(arg_file_path)[-1]
     assert(type(arg_file) is str)
     arg_file_prefix = arg_file[:-4]
     print(arg_file_prefix)
     log_file = f"logs/{arg_file_prefix}.log"
     err_file = f"logs/{arg_file_prefix}.err"
-
+    if os.path.exists(log_file) == True:
+        assert False, f"log file {log_file} exists and should not be overwrited"
     cmd = f"(mpiexec -n {kernel_num} python DeepMimic_Optimizer.py --arg_file {arg_file_path} 1>{log_file} 2>{err_file} &)"
     cmd_lst.append(cmd)
     print(f"arg file {arg_file_path}\ncmd {cmd}")
     os.popen(cmd)
-
