e3917d0841d78c54f255937c577b24ab152c1a2c
rullec <1009694460@qq.com>
2021-01-27 15:44:21 +0800

    tmp save

--- batch_run.sh
+++ batch_run.sh
@@ -1,3 +1,2 @@
-# (python -u DeepMimicTorch_Optimizer.py --arg_file ./args/0126/have_noise/train_2links.txt 1>havenoise.log 2>&1 &)
-# (python -u DeepMimicTorch_Optimizer.py --arg_file ./args/0126/no_noise/train_2links.txt 1>nonoise.log 2>&1 &)
-
+(python -u DeepMimicTorch_Optimizer.py --arg_file ./args/0126/train_no_noise.txt 1>no_noise.log 2>&1 &)
+(python -u DeepMimicTorch_Optimizer.py --arg_file ./args/0126/train_have_noise.txt 1>have_noise.log 2>&1 &)
